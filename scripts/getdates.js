8481a4b36aca177232d59cde9bcf618f7eee7c2d
pratiwi9966 <wspratiwi2009@gmail.com>
2025-11-03 07:45:50 -0700

    adjsted the comment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -6,7 +6,7 @@ const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 
 // Output the current year to the footer’s first <p> element
-//    (The <span id="currentYear">...</span> in your HTML)
+//    (The <span id="currentYear">...</span> in HTML)
 document.getElementById("currentYear").textContent = currentYear;
 
 // Output the document’s last modified date/time
