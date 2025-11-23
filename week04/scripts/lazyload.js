07b7b020b2629c58e838349cab15a539f102dfb5
pratiwi9966 <wspratiwi2009@gmail.com>
2025-11-22 23:08:35 -0700

    Activity 3

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,15 @@
+
+//Footer-------------------------------------
+// Create a new Date object for the current date and year
+const currentDate = new Date();
+
+// get the current year using "getFullYear()""
+const currentYear = currentDate.getFullYear();
+
+// Show the current year to the footer’s first <p> element
+//    (The <span id="currentYear">...</span> in HTML)
+document.getElementById("currentYear").textContent = currentYear;
+
+// Show the document’s last modified date/time
+//    (Using the built-in document.lastModified property)
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
