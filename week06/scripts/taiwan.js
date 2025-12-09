5aea23ab1c8bed7ce1ea89453f3766a273263153
pratiwi9966 <wspratiwi2009@gmail.com>
2025-12-09 01:26:49 -0700

    w6 project

---
+++ week06/scripts/taiwan.js
@@ -0,0 +1,42 @@
+
+/* ============================================================
+   Responsive Navigation Menu (Hamburger Menu)
+   ============================================================ */
+
+// Select the hamburger button   
+const hamButton = document.querySelector('#menu');
+// Select the nav element
+const navigation = document.querySelector('nav');
+// Select the gallery element
+const gallery = document.querySelector('#gallery');
+
+// Add a click event to open/close the navigation menu
+hamButton.addEventListener('click', () => {
+  navigation.classList.toggle('open');// Show/hide the menu
+  hamButton.classList.toggle('open');// Change icon (☰ ↔ ❌)
+});
+
+
+
+
+
+
+
+
+
+/* ============================================================
+   Footer: Year + Last Modified
+   ============================================================ */
+// Create a new Date object for the current date and year
+const currentDate = new Date();
+
+// get the current year using "getFullYear()""
+const currentYear = currentDate.getFullYear();
+
+// Show the current year to the footer’s first <p> element
+//    (The <span id="currentYear">...</span> in HTML)
+document.getElementById("currentYear").textContent = currentYear;
+
+// Show the document’s last modified date/time
+//    (Using the built-in document.lastModified property)
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
